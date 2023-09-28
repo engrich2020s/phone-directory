d02436df1b83d822105e5fb81bcaab7f26ddb516
Figueroa, Aldrich <aldrich.figueroa@accenture.com>
2023-09-28 12:02:45 +0800

    Phone directory validators

--- src/app/phone-directory/phone-directory.component.ts
+++ src/app/phone-directory/phone-directory.component.ts
@@ -9,30 +9,14 @@ import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/fo
 export class PhoneDirectoryComponent {
   contactForm: FormGroup;
   contactList: { name: string; mobileNumber: number; email: string }[] = [];
+
   constructor(private formBuilder: FormBuilder) {
     this.contactForm = this.formBuilder.group({
       name: ['', [Validators.required, Validators.pattern('^[A-Za-z]+$')]],
       mobileNumber: ['', [Validators.required, Validators.maxLength(10), Validators.pattern('^[0-9]+$')]],
-      email:['', [Validators.required, Validators.pattern(/^[a-zA-Z][a-zA-Z0-9.]{1,9}@[a-zA-Z]{2,20}\.[a-zA-Z]{2,10}$/), this.emailValidator]]
+      email:['', [Validators.required, Validators.pattern(/^[a-zA-Z][a-zA-Z0-9.]{1,9}@[a-zA-Z]{2,20}\.[a-zA-Z]{2,10}$/)]],
     });
   }
-
-  emailValidator(control: AbstractControl) {
-    const email = control.value;
-    const parts = email.split('@');
-    if (parts.length === 2) {
-      const username = parts[0];
-      const domParts = parts[1].split('.');
-      if (username.length >= 2 && username.length <= 10 && 
-          domParts.length === 2 && domParts[0].length >=2 && domParts[0].length <= 10 &&
-          domParts[1].length >=2 && domParts[1].length <= 20 &&
-          /^[a-zA-Z]+$/.test(domParts[0]) &&
-        /^[a-zA-Z]+$/.test(domParts[1])) {
-        return null;
-      }
-    }
-    return { invalidEmail: true }
-}
 
   submitForm() {
     if (this.contactForm.valid) {
